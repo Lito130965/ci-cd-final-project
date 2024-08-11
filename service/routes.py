73ede0490aa4fcb806a4f7cf917fae0b841f561c
lito130965 <lito130965@gmail.com>
2024-08-11 10:05:46 -0400

    PEP8 fix

--- service/routes.py
+++ service/routes.py
@@ -2,7 +2,7 @@
 Controller for routes
 """
 from flask import jsonify, url_for, abort
-from service import app
+from flask import current_app as app
 from service.common import status
 
 COUNTER = {}
